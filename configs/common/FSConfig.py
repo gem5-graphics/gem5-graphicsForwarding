f8b5f9edf1eb9fbcf037aa7b385493574f07edd7
Ayub A. Gubran <ayoubg@ece.ubc.ca>
2018-04-14 00:38:22 -0700

    add option for nougat with graphics forwarding
    Change-Id: I0ca5fe87d6a2e8400542fdbda6ccc561b1f8539b

--- configs/common/FSConfig.py
+++ configs/common/FSConfig.py
@@ -357,6 +357,15 @@ def makeArmSystem(mem_mode, machine_type, num_cpus=1, mdesc=None,
             if 'kitkat' in mdesc.os_type():
                 cmdline += " androidboot.hardware=gem5 qemu=1 qemu.gles=0 " + \
                            "android.bootanim=0 "
+            elif 'nougat-gf' in mdesc.os_type():
+                cmdline += " androidboot.hardware=gem5 qemu=1 qemu.gles=1 " + \
+                           "qemu.opengles.version=196608 " + \
+                           "cma=128M " + \
+                           "vmalloc=640MB " + \
+                           "android.early.fstab=/fstab.gem5 " + \
+                           "androidboot.selinux=permissive " + \
+                           "audit=0 " + \
+                           "android.bootanim=0 "
             elif 'nougat' in mdesc.os_type():
                 cmdline += " androidboot.hardware=gem5 qemu=1 qemu.gles=0 " + \
                            "android.bootanim=0 " + \
